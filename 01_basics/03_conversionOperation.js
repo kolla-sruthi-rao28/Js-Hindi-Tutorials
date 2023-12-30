831d25d6a538d022dadb1f227b023173212ebad4
kolla-sruthi-rao28 <51354925+kolla-sruthi-rao28@users.noreply.github.com>
2023-12-30 15:30:20 +0000

    dataType Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+// let score = 50;
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
+
+// let score = '33';
+// console.log(typeof score); // string
+// console.log(typeof (score)); // string
+
+// let valueInNumber = Number(score); // where score: '33'
+// console.log(typeof valueInNumber);// number
+
+// let score2 = "33a"
+// let score = Number(score2);
+// console.log(typeof score2); // String
+// console.log(typeof score);// number
+// console.log(score); //NaN
+
+// let nullValue = null;
+// let nullValueNumber = Number(nullValue);
+// console.log(typeof nullValue); // object
+// console.log(typeof nullValueNumber); //number
+// console.log(nullValueNumber); //0
+
+
+// let undefinedValue = undefined;
+// let undefinedValueNumber = Number(undefinedValue);
+// console.log(typeof undefinedValue); // undefined
+// console.log(typeof undefinedValueNumber); //number
+// console.log(undefinedValueNumber); //NaN
+
+// NOtes
+// Number Conversation
+        // "33" => 33
+        // "33abc" => NaN (Not a Number)
+        // true => 1 
+        // false => 0
+// boolean Conversation:
+    // let isLoggedIn =  1
+    // let isLoggedInBoolean = Boolean(isLoggedIn);
+    // console.log(isLoggedInBoolean); // true
+    // console.log(typeof isLoggedInBoolean); // boolean
+
+        // 1 => true
+        // 0 => false
+        // "" => false
+        // "Kolla" = > true
+
+// String Conversation:
+    let stringNumberConversation = String(22)
+    console.log(stringNumberConversation); // 22
+    console.log(typeof stringNumberConversation) // string
+    
+
+
+
