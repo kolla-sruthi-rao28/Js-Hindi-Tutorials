62c5221ab8ff7d2d660b916cddb098c610846129
kolla-sruthi-rao28 <51354925+kolla-sruthi-rao28@users.noreply.github.com>
2023-12-30 15:08:31 +0000

    dataTypes file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3); // we are using node js not the browser
+
+//code readability should be high
+
+// mdn web docs
+
+//tc39.cs/ecma262
+
+//datatypes:
+
+let name = 'kolla';
+let age = 20;
+let isLoggedIn = false;
+
+//Primitive datatypes:
+
+// Number
+// BigInt
+// String
+// boolean
+// null
+// undefined
+// Symbol => unique
+
+
+console.log(typeof "Sruthi") // string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol); // function
+
