581224bfa9cdebfde63eb4e6793442441c037409
kolla-sruthi-rao28 <51354925+kolla-sruthi-rao28@users.noreply.github.com>
2023-12-30 15:54:46 +0000

    Type Conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,10 +46,30 @@
         // "Kolla" = > true
 
 // String Conversation:
-    let stringNumberConversation = String(22)
-    console.log(stringNumberConversation); // 22
-    console.log(typeof stringNumberConversation) // string
-    
+    // let stringNumberConversation = String(22)
+    // console.log(stringNumberConversation); // 22
+    // console.log(typeof stringNumberConversation) // string
 
+let value = 3;
+let negValue = -value;
+console.log(negValue); // -3
+
+//Operators:
+    // +
+    // -
+    // *
+    // /
+    // %
+
+let str1 = "hello";
+let str2 = " Kolla";
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); 122
+console.log(1+2+"2"); //32
+console.log(+true); //1
+console.log(+""); //0
 
 
