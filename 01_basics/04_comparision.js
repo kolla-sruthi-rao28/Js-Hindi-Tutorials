ae336bf945ab4414fbf30b6f926eab6e82f3100e
kolla-sruthi-rao28 <51354925+kolla-sruthi-rao28@users.noreply.github.com>
2023-12-31 10:34:39 +0000

    comparision file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1); // true
+// console.log(2>=1); // true
+// console.log(2<1); // false
+// console.log(2==1); // false
+// console.log(2!=1); //true
+// console.log('2'>1); //true
+// console.log('02'>1); // true
+// console.log(null>0); // false
+// console.log(null == 0); // false
+// console.log(null>=0); // true
+// console.log(undefined == 0); // false
+// console.log(undefined>0); // false
+// console.log(undefined<0); // false
+
+// console.log(Number(null)); //0
+// console.log(Number(undefined)); // NaN
+
+// Convert null to a number treating it as 0. null>=0 (true), null>0 (false)
+// Comparision & equality check work differently.
+// console.log('2' === 2);
